e70b02072f9ec3a5ad919151d5da1ae689ca8292
Habibul Rahman <habibul.rahman@lndata.com>
2022-04-11 19:35:08 +0700

    Added Model

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,21 +1,31 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    print(request.GET)
-    print(request.POST)
-    body = request.body
-    print(body)
+    model_data = Product.object.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    #print(data.keys())
-    print(data)
-    
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
-
+    if model_data:
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
+
+
+    # print(request.GET)
+    # print(request.POST)
+    # body = request.body
+    # print(body)
+    # data = {}
+    # try:
+    #     data = json.loads(body)
+    # except:
+    #     pass
+    # #print(data.keys())
+    # print(data)
+    
+    # data['params'] = dict(request.GET)
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
+
+    # return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ import requests
 #endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc": 123}, json={"query" : "Hello World"})
+get_response = requests.get(endpoint, json={"query" : "Hello World"})
 
 #print(get_response.text)
 print(get_response.json())
